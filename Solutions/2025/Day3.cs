2abc67a936a8af295e2ae81cf5852f146b0c30dd
georgeo <georgeo@pulsonix.com>
2025-12-03 16:45:49 +0000

    Complete 2025 - day 3 - parts 1 and 2

--- Solutions/2025/Day3.cs
+++ Solutions/2025/Day3.cs
@@ -10,19 +10,50 @@ namespace Solutions.Year2025
 {
     public partial class Lobby
     {
-        [GeneratedRegex(@"(\d+)-(\d+),*")]
-        public static partial Regex SplitProductIdsRegex();
+        [GeneratedRegex(@"(\d+)")]
+        public static partial Regex SplitBatteryBanks();
+
+        private static string MaxStringOfNChars(List<char> chars, int n)
+        {
+            string finalString = "";
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                char nextMax = chars.SkipLast(i).Max();
+                int indexOfNextMax = chars.IndexOf(nextMax);
+                chars = [.. chars.Skip(indexOfNextMax + 1)];
+                finalString += nextMax;
+            }
+
+            return finalString;
+        }
 
         public static long Part1(string input)
         {
-            long total = 0;
-            return total;
+            long voltageSum = 0;
+            const int numOfDigits = 2;
+
+            foreach (Match match in SplitBatteryBanks().Matches(input))
+            {
+                List<char> batteryBank = [.. match.Value.Select(c => c)];
+                voltageSum += long.Parse(MaxStringOfNChars(batteryBank, numOfDigits));
+            }
+
+            return voltageSum;
         }
 
         public static long Part2(string input)
         {
-            long total = 0;
-            return total;
+            long voltageSum = 0;
+            const int numOfDigits = 12;
+
+            foreach (Match match in SplitBatteryBanks().Matches(input))
+            {
+                List<char> batteryBank = [.. match.Value.Select(c => c)];
+                voltageSum += long.Parse(MaxStringOfNChars(batteryBank, numOfDigits));
+            }
+
+            return voltageSum;
         }
     }
 }
